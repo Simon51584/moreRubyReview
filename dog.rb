ccb94f673e6cd91f14af92db80f196b40823eab5
Simon51584 <simon51584@gmail.com>
2021-03-11 19:41:09 -0600

    fixed dog class and put together an instance method to make each do bark

--- dog.rb
+++ dog.rb
@@ -1,17 +1,22 @@
 class Dog 
    
-    def talk
-        Speak = "bark"
+    def bark
+        puts "Woof"
     end
-    
+
 
 end 
 
 
 
 fido = Dog.new
+fido.bark
+
 snoopy = Dog.new
+snoopy.bark
+
 lassie = Dog.new
+lassie.bark
 
 
 
